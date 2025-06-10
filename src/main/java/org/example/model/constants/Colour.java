c36398c19d1cc04439a61722086dcc5dec16104f
Ekaterina Dzhedzheia <kattykitty21@gmail.com>
2025-06-10 13:08:59 +0500

    Очистил main: оставил только README.md

--- src/main/java/org/example/model/constants/Colour.java
+++
@@ -1,6 +0,0 @@
-package org.example.model.constants;
-
-public class Colour {
-    public static final String RED = "red";
-    public static final String GREEN = "green";
-}
--- src/main/java/org/example/model/constants/Discount.java
+++
@@ -1,5 +0,0 @@
-package org.example.model.constants;
-
-public class Discount {
-    public static final double SIXTY = 60.0;
-}
